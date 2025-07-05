8e02ffdf0082ba8c52cc7aebdf07224191706dcc
Adam <adamomarali37@gmail.com>
2025-07-05 19:07:06 -0400

    fix for cors to allow nextjs site

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -5,7 +5,10 @@ import "net/http"
 func EnableCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:3000" || origin == "https://foundrymemory.vercel.app" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
